37f9c65732fc57460ed9104353a8f9bd2e69ee4c
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2024-05-10 11:07:43 +0300

    Time: 1 ms (99.28%) | Memory: 44.9 MB (54.18%) - LeetSync

---
+++ 00LeetCodeProblemset/412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> results = new ArrayList<>(n);
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    results.add("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    results.add("Fizz");
+                } else if (i % 5 == 0) {
+                    results.add("Buzz");
+                } else {
+                    results.add(String.valueOf(i));
+                }
+            }
+            
+            return results;
+    }
+}
