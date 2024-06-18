2b9756ac4fb48f048e3cce73b908424c1207def5
lubomir-m <122978156+lubomir-m@users.noreply.github.com>
2024-06-18 08:54:23 +0300

    Time: 0 ms (100.00%) | Memory: 41.5 MB (18.35%) - LeetSync

--- 03-Array-and-String/54-spiral-matrix/spiral-matrix.java
+++ 03-Array-and-String/54-spiral-matrix/spiral-matrix.java
@@ -2,10 +2,10 @@ class Solution {
      public List<Integer> spiralOrder(int[][] matrix) {
             int n = matrix.length;
             int m = matrix[0].length;
-            List<Integer> result = new ArrayList<>(n * m);
+            List<Integer> list = new ArrayList<>(n * m);
             boolean[][] visited = new boolean[n][m];
-            traverse(0, 0, n, m, matrix, visited, result);
-            return result;
+            traverse(0, 0, n, m, matrix, visited, list);
+            return list;
         }
 
         private void traverse(int row, int col, int n, int m, int[][] matrix, boolean[][] visited, List<Integer> result) {
@@ -19,6 +19,7 @@ class Solution {
             if (row + 1 >= n || visited[row + 1][col]) {
                 traverse(row, col - 1, n, m, matrix, visited, result);
             }
+            
             if (col - 1 < 0 || visited[row][col - 1]) {
                 traverse(row - 1, col, n, m, matrix, visited, result);
             }
