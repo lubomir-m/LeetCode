55e29bf7321592834a75748e80e3c5ff3efaa9f8
lubomir-m <122978156+lubomir-m@users.noreply.github.com>
2024-06-29 12:24:16 +0300

    Time: 1 ms (100.00%) | Memory: 42.1 MB (61.61%) - LeetSync

--- 03-Array-and-String/67-add-binary/add-binary.java
+++ 03-Array-and-String/67-add-binary/add-binary.java
@@ -1,22 +1,36 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int n = a.length(), m = b.length();
-        if (n < m) return addBinary(b, a);
+            int n = a.length();
+            int m = b.length();
+            if (m > n) {
+                return addBinary(b, a);
+            }
 
-        StringBuilder sb = new StringBuilder();
-        int carry = 0, j = m - 1;
-        for (int i = n - 1; i > -1; --i) {
-            if (a.charAt(i) == '1') ++carry;
-            if (j > -1 && b.charAt(j--) == '1') ++carry;
+            StringBuilder sb = new StringBuilder();
+            int carry = 0;
+            int j = m - 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (a.charAt(i) == '1') {
+                    carry++;
+                }
+                if (j >= 0 && b.charAt(j) == '1') {
+                    carry++;
+                }
 
-            if (carry % 2 == 1) sb.append('1');
-            else sb.append('0');
+                if (carry % 2 == 1) {
+                    sb.append('1');
+                } else {
+                    sb.append('0');
+                }
 
-            carry /= 2;
+                carry /= 2;
+                j--;
+            }
+
+            if (carry == 1) {
+                sb.append('1');
+            }
+
+            return sb.reverse().toString();
         }
-        if (carry == 1) sb.append('1');
-        sb.reverse();
-
-        return sb.toString();
-    }
 }
