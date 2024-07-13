bbc2499bab81e651fdc342ac23c22ab2444a5e47
lubomir-m <122978156+lubomir-m@users.noreply.github.com>
2024-07-13 18:32:13 +0300

    Time: 2 ms (27.69%) | Memory: 57.4 MB (40.71%) - LeetSync

--- 03-Array-and-String/189-rotate-array/rotate-array.java
+++ 03-Array-and-String/189-rotate-array/rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int[] a = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            a[(i + k) % nums.length] = nums[i];
+     public void rotate(int[] nums, int k) {
+            int[] array = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                array[(i + k) % nums.length] = nums[i];
+            }
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = array[i];
+            }
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = a[i];
-        }
-    }
 }
